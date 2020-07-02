b2c2f816ba8a23414ff1face1b5a4f0922fad063
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-01 19:53:38 -0700

    Commiting change

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -1,4 +1,19 @@
+import uniqid from 'uniqid';
 
+export default class List {
+  constructor() {
+    this.items = [];
+  }
+  addItem (count, unit, ingredient) {
+    const item = {
+      id: uniqid(),
+      count,
+      unit,
+      ingredient
+    }
+    this.items.push(item);
+    return item;
+  }
 
   deleteItem (id) {
     const index = this.items.findIndex(el => el.id === id);
