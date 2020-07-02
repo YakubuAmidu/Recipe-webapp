00d7f09d85e33446296224c9e09b7d9ad4be7290
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-01 19:55:29 -0700

    Commiting changes

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -15,4 +15,14 @@ export default class List {
     return item;
   }
 
-  
+  deleteItem (id) {
+    const index = this.items.findIndex(el => el.id === id);
+    // [2, 4, 8] splice(1, 2) -> returns [4, 8],  original array is [2]
+    // [2, 4, 8] slice(1, 2) -> returns 4, original array is [2, 4, 8]
+    this.items.splice(index, 1);
+  }
+
+  updateCount(id, newCount) {
+    this.items.find(el => el.id === id).count = newCount;
+  }
+}
