8e3a712ac0e60eb6da42bcc55b99ea7f802d8cab
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-01 19:28:59 -0700

    Commiting Recipe webapp

---
+++ src/js/models/Search.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { key, proxy } from '../config';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+     async getResults() {
+       try {
+       const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+       this.result = res.data.recipes;
+       // console.log(this.result);
+       } catch (error){
+          alert(error);
+       }
+  }
+}
+
+
+
+
+
+//export default 'I am an exported string.';
+//const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
