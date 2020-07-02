f461adc6b689d68e9b6ca86e418736e7f66209bf
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-01 19:51:45 -0700

    Commiting changes

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -18,3 +18,23 @@ export default class Likes {
     // Persist data in localStorage
     this.persistData();
   }
+
+    isLiked(id) {
+      return this.likes.findIndex(el => el.id === id) !== -1;
+    }
+
+    getNumLikes() {
+      this.likes.length;
+    }
+
+    persistData() {
+      localStorage.setItem('likes', JSON.stringify(this.likes));
+    }
+
+    readStorage() {
+      const storage = JSON.parse(localStorage.getItem('likes'));
+
+      // Restoring likes from the localStorage
+      if (storage) this.likes = storage;
+    }
+  }
