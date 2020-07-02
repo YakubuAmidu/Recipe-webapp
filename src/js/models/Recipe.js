7805925197635e9e20bdadfb31b61757e58cada5
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-01 20:00:27 -0700

    Commiting changes

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -6,7 +6,30 @@ export default class Recipe {
     this.id = id
   }
 
-  
+  async getRecipe() {
+    try {
+      const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+        this.title = res.data.recipe.title;
+        this.author = res.data.recipe.publisher;
+        this.img = res.data.recipe.image_url;
+        this.url = res.data.recipe.source_url;
+        this.ingredients = res.data.recipe.ingredients;
+    } catch(error) {
+      console.log(error);
+      alert('Something went wrong :(');
+    }
+  }
+
+  calcTime() {
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
+
+  calcServings() {
+    this.servings = 4;
+  }
+
   parseIngredients() {
     const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
     const unitsShort = ['tbsp', 'btsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
