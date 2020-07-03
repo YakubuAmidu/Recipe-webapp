5d2c707f7bf5a8f4aad75c655136d6253493fc01
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-02 23:34:43 -0700

    Commiting changes

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -6,7 +6,16 @@ export default class Search {
     this.query = query;
   }
 
-     
+  async getResults() {
+    try {
+    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+    this.result = res.data.recipes;
+    // console.log(this.result);
+    } catch (error){
+       alert(error);
+    }
+}
+}
 
 
 
