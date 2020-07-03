270958815837b835b6c916d65ed70f75d3186ca3
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-02 23:35:47 -0700

    Commiting changes

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,14 +1,3 @@
-export const elements = {
-searchForm: document.querySelector('.search'),
-  searchInput: document.querySelector('.search__field'),
-  searchRes: document.querySelector('.results'),
-  searchResList: document.querySelector('.results__list'),
-  searchResPages: document.querySelector('.results__pages'),
-  recipe: document.querySelector('.recipe'),
-  shopping: document.querySelector('.shopping__list'),
-  likesMenu: document.querySelector('.likes__field'),
-  likesList: document.querySelector('.likes__list')
-};
 
 export const elementStrings = {
   loader: 'loader'
