8584cbc6c73997500393851491a242126c3608cb
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-02 23:43:40 -0700

    Commiting changes

--- src/js/views/likesView.js
+++ src/js/views/likesView.js
@@ -11,7 +11,22 @@ export const toggleLikeMenu = numLikes => {
   elements.likesMenu.style.visibility = numLikes > 0 ? 'visible' : 'hidden';
 };
 
-
+export const renderLike = like => {
+  const markup = `
+  <li>
+      <a class="likes__link" href="#${like.id}">
+          <figure class="likes__fig">
+              <img src="${like.img}" alt="${like.title}">
+          </figure>
+          <div class="likes__data">
+              <h4 class="likes__name">${limitRecipeTitle(like.title)}</h4>
+              <p class="likes__author">${like.author}</p>
+          </div>
+      </a>
+  </li>
+  `;
+  elements.likesList.insertAdjacentHTML('beforeend', markup);
+};
 
 export const deleteLike = id => {
   const el = document.querySelector(`.likes__link[href*="${id}"]`).parentElement;
