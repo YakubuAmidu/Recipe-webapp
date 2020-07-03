db514ef35de4a9c005bd493f6453c23025168263
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-02 23:38:00 -0700

    Commiting changes

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -9,23 +9,3 @@ searchForm: document.querySelector('.search'),
   likesMenu: document.querySelector('.likes__field'),
   likesList: document.querySelector('.likes__list')
 };
-
-export const elementStrings = {
-  loader: 'loader'
-};
-
-export const renderLoader = parent => {
-  const loader = `
- <div class="${elementStrings.loader}">
-  <svg>
-     <use href="img/icons.svg#icon-cw"></svg>
-  </svg>
- </div>
-  `;
-  parent.insertAdjacentHTML('afterbegin', loader);
-};
-
-export const clearLoader = () => {
-  const loader = document.querySelector(`.${elementStrings.loader}`);
-  if (loader) loader.parentElement.removeChild(loader);
-};
