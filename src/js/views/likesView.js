e057bccf9f343fcbf525756fe41964bda2bf904f
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-02 23:44:38 -0700

    Commiting changes

--- src/js/views/likesView.js
+++ src/js/views/likesView.js
@@ -27,8 +27,3 @@ export const renderLike = like => {
   `;
   elements.likesList.insertAdjacentHTML('beforeend', markup);
 };
-
-export const deleteLike = id => {
-  const el = document.querySelector(`.likes__link[href*="${id}"]`).parentElement;
-  if (el) el.parentElement.removeChild(el);
-}
