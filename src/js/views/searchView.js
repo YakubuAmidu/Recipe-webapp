c2be1798f45091b01cd18bff39e666171078e3ae
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-04 11:18:06 -0700

    Commiting changs

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,51 +1,12 @@
 
+import { elements } from './base';
 
-export const clearResults = () => {
-  elements.searchResList.innerHTML = '';
-  elements.searchResPages.innerHTML = '';
+export const getInput = () => elements.searchInput.value;
+
+export const clearInput = () => {
+  elements.searchInput.value = '';
 };
 
-export const highlightSelected = id => {
-const resultsArr = Array.from(document.querySelectorAll('.results__link'));
-resultsArr.forEach(el => {
-  el.classList.remove('results__link--active');
-});
-
-document.querySelector(`.results__link[href*="${id}"]`).classList.add('results__link--active');
-};
-
-// 'Pasta with tomato and spinach'
-export const limitRecipeTitle = (title, limit = 17) => {
-  const newTitle = [];
-  if (title.length > limit) {
-    title.split(' ').reduce((acc, cur) => {
-      if (acc + cur.length <= limit) {
-         newTitle.push(cur);
-      }
-      return acc + cur.length;
-    }, 0);
-
-    return `${newTitle.join(' ')} ...`;
-  }
-  return title;
-}
-
-const renderRecipe = recipe => {
-   const markup = `
-   <li>
-       <a class="results__link" href="#${recipe.recipe_id}">
-           <figure class="results__fig">
-               <img src="${recipe.image_url}" alt="${recipe.title}">
-           </figure>
-           <div class="results__data">
-               <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
-               <p class="results__author">${recipe.publisher}</p>
-           </div>
-       </a>
-   </li>
-   `;
-   elements.searchResList.insertAdjacentHTML('beforeend', markup);
-};
 
 // type 'prev' or 'next'
 const createButton = (page, type) => `
